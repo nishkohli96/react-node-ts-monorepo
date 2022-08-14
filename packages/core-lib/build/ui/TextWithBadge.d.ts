8d162b0069e89fcd96ed1a10e586532498413384
Nishant Kohli <nishkohli96@gmail.com>
2022-08-14 20:17:41 +0530

    fix missing turbo scripts

---
+++ packages/core-lib/build/ui/TextWithBadge.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+interface TextWithBadgeProps {
+    text: string;
+    badgeVal: number;
+}
+declare const TextWithBadge: (props: TextWithBadgeProps) => JSX.Element;
+export default TextWithBadge;
