76bf3b967ab716d48481b58f4c2ff1e764c150e5
Nishant Kohli <nishkohli96@gmail.com>
2022-08-17 03:38:27 +0530

    build frontend success

--- apps/fastify-server/src/main.ts
+++ apps/fastify-server/src/main.ts
@@ -3,17 +3,16 @@ import {
 	FastifyAdapter,
 	NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import compression from '@fastify/compress';
+import pino from 'pino';
+import pretty from 'pino-pretty';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestFastifyApplication>(
 		AppModule,
 		new FastifyAdapter({
-			logger: {
-				level: 'info',
-				prettyPrint: true,
-			},
+			logger: pino(pretty()), // remove in prod
+			/* https://github.com/pinojs/pino-pretty#integration */
 			ignoreTrailingSlash: true,
 			caseSensitive: false,
 		}),
@@ -22,9 +21,17 @@ async function bootstrap() {
 		}
 	);
 
-	app.register(compression, { encodings: ['gzip', 'deflate'] });
-
-	await app.listen(4000);
+	/**
+	 * Unfortunately NestJs prod build with fastify, works on local machine,
+	 * but is not working on docker...
+	 *
+	 * Soln link -
+	 * https://stackoverflow.com/questions/66086427/docker-container-with-nodejs-appnestjs-is-not-accessible-from-both-other-conta
+	 *
+	 * To use the express version -
+	 * const app = await NestFactory.create(AppModule);
+	 */
+	await app.listen(4000, '0.0.0.0');
 }
 
 bootstrap();
