cc37a3e550cacbd182a0fd8b63a56fa60e414f68
Nishant <nishkohli96@gmail.com>
2024-02-12 01:58:16 +0530

    next-client done

--- apps/frontend/src/redux-store/index.ts
+++ apps/frontend/src/redux-store/index.ts
@@ -2,3 +2,4 @@ export * from './hooks';
 export * from './reducers';
 export * from './services';
 export * from './store';
+export * from './types';
--- apps/frontend/src/redux-store/store.ts
+++ apps/frontend/src/redux-store/store.ts
@@ -39,5 +39,3 @@ export const store = configureStore({
 });
 
 export const persistor = persistStore(store);
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
