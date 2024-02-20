2c46b39a3c67e9287ab3ef199f91d3fe3cd13965
Nishant <nishkohli96@gmail.com>
2024-02-21 03:04:11 +0530

    rm metadata from page.tsx in next-client

--- apps/next-client/src/app/pokemon/[id]/page.tsx
+++ apps/next-client/src/app/pokemon/[id]/page.tsx
@@ -1,11 +1,3 @@
-import type { Metadata } from 'next';
-
-export const metaData: Metadata = {
-  title: 'Pokemon List',
-  description: 'fetch pokemon list',
-  keywords: 'hi',
-};
-
 export default function PokemonList() {
   return (
     <main>
