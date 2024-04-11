d7fa810b2822d552ac66da0937564865804d07c6
Nishant <nishkohli96@gmail.com>
2024-04-12 01:40:53 +0530

    upgrade @nish1896/eslint-config

--- apps/frontend/.eslintrc.js
+++ apps/frontend/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@nish1896'],
+  extends: ['@nish1896/eslint-config/js', '@nish1896/eslint-config/react'],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
   }
--- apps/next-client/.eslintrc.js
+++ apps/next-client/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     'next/core-web-vitals',
-    '@nish1896'
+    '@nish1896/eslint-config/js',
+    '@nish1896/eslint-config/react'
   ],
 }
