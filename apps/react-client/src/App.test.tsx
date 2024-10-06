227d2d097cf8969a03e90ee5b545700a9583b381
Nishant <nishkohli96@gmail.com>
2024-10-07 02:14:33 +0530

    upgrade react dependencies

--- apps/react-client/src/App.test.tsx
+++ apps/react-client/src/App.test.tsx
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+/**
+ * Need to fix..failing due to redux
+ */
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
