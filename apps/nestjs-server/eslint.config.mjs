cf49df946f3ca01c8c6f4749be17ca228607ec82
Nishant <nishkohli96@gmail.com>
2025-03-30 23:18:49 +0530

    lint nestjs-server

--- apps/nestjs-server/eslint.config.mjs
+++ apps/nestjs-server/eslint.config.mjs
@@ -32,7 +32,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@stylistic/plus/curly-newline': ['warn', { consistent: true }],
     },
   },
 );
--- apps/nestjs-server/src/routes/home/controller.spec.ts
+++ apps/nestjs-server/src/routes/home/controller.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, type TestingModule } from '@nestjs/testing';
 import { HomeController } from './controller';
 import { HomeService } from './service';
 
